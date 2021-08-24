9ca5f3bcce57877ca97d88f1f46572e8306929ad
sksinghjhs16 <sksingh.jhs@gmail.com>
2021-08-24 16:59:08 +0530

    initial commit

--- src/components/restaurant/CompactResInfo.js
+++ src/components/restaurant/CompactResInfo.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 const CompactResInfo = ({ restaurant }) => {
   return (
     <View>
-      <Text>hi</Text>
+      <Text>himfhgfhgfh</Text>
     </View>
   );
 };
